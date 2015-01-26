15d818c6977e5e96a94aacaf59cc8b71cc595790
chinaclp <1048900587@qq.com>
2015-01-26 12:05:31 +0800

    sns建立

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sns::Application.config.secret_token = '0c5f9c5659328cc36736d475cb2f7e9b9838dc5b3a794a263939ddfe9eee43e74eed37714891aaf9fd59bdd5e93b36b83ca4fe1a570369e52194d49716782b22'
